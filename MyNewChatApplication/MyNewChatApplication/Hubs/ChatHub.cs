b3e4c9aaa29f519fa0c44d0f3281b7aff9e60176
mohidul31 <mislam6789@gmail.com>
2018-07-18 23:37:25 +0600

    MyNewChatApplication Added Fot IIT

---
+++ MyNewChatApplication/MyNewChatApplication/Hubs/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace MyNewChatApplication.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.addNewMessageToPage(name, message);
+        }
+
+        public void SendFromTo(string from, string message, string to)
+        {
+            //Clients.Client(connId).appendNewMessage(name, message);  
+            Clients.All.addNewMessageToPage(from, message, to);
+        }
+    }
+}
